27b60dd74ba9562afd8edada2daf3c019dade515
Daniel Quek <daniel.tianwen@gmail.com>
2012-02-12 19:13:59 +0800

    added special component

---
+++ Budget Barbie/SpecialItem.h
@@ -0,0 +1,13 @@
+//
+//  SpecialItem.h
+//  Budget Barbie
+//
+//  Created by Daniel Quek on 12/2/12.
+//  Copyright (c) 2012 Cellcity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SpecialItem : NSObject
+
+@end
---
+++ Budget Barbie/SpecialItem.m
@@ -0,0 +1,13 @@
+//
+//  SpecialItem.m
+//  Budget Barbie
+//
+//  Created by Daniel Quek on 12/2/12.
+//  Copyright (c) 2012 Cellcity. All rights reserved.
+//
+
+#import "SpecialItem.h"
+
+@implementation SpecialItem
+
+@end
