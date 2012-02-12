bd57ed90c7dc3ec4ee569042cf105116d3bdabb6
Daniel Quek <daniel.tianwen@gmail.com>
2012-02-12 08:58:15 +0800

    feb 12

---
+++ Budget Barbie/SpecialHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SpecialHomeViewController.h
+//  Budget Barbie
+//
+//  Created by Daniel Quek on 12/2/12.
+//  Copyright (c) 2012 Cellcity. All rights reserved.
+//
+
+#import "CustomTitleViewController.h"
+
+@interface SpecialHomeViewController : CustomTitleViewController
+
+@end
